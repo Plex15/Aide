ec3d58ad8dd884102933ce36458792965c0d9a9c
Plex@15 <joshualkj6121@gmail.com>
2025-09-28 11:17:17 +0530

    fix some gradlew compile issue and debug apk compiled to test

--- frontend/android/app/src/main/java/com/aide/MainActivity.kt
+++ frontend/android/app/src/main/java/com/aide/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.aide
 
+// ADD THESE IMPORT STATEMENTS AT THE TOP
+import android.os.Bundle
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import com.baekgol.reactnativealarmmanager.util.BootReceiver // Import BootReceiver from the library
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +25,20 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  // THIS IS THE CORRECTED KOTLIN CODE BLOCK
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // It's important to call super.onCreate first, but in some RN versions, it must come AFTER the delegate is created.
+    // The default template calls it in the delegate, so we call super.onCreate(null) to avoid state restoration issues.
+    super.onCreate(null) 
+
+    val receiver = ComponentName(this, BootReceiver::class.java)
+    val packageManager: PackageManager = this.packageManager
+
+    packageManager.setComponentEnabledSetting(
+      receiver,
+      PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+      PackageManager.DONT_KILL_APP
+    )
+  }
 }
